5a3bb7ae1aa1b83bd01f279d826b3a399f749eef
tuanzi0103 <168818159+tuanzi0103@users.noreply.github.com>
2025-10-01 14:21:10 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from charts.inventory import show_inventory
 from charts.product_mix_only import show_product_mix_only
 from charts.customer_segmentation import show_customer_segmentation
 from services.ingestion import ingest_excel, ingest_csv, init_db_from_drive_once
-from services.init_db import init_db
+from init_db import init_db
 from services.ingestion import init_db_from_drive_once
 import os
 os.environ["WATCHDOG_DISABLE_FILE_WATCH"] = "true"
